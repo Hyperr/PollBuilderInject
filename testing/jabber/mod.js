27fb120f36ddb850db4c4dd5fe22dbe3c632d8da
Bryan Grezeszak <grezeszakb@gmail.com>
2018-04-11 15:30:35 -0400

    ...

--- testing/jabber/mod.js
+++ testing/jabber/mod.js
@@ -1,4 +1,4 @@
-console.log('v 2');
+console.log('v 4');
 var doEmbed = PollBuilderInject.map('body.template-collection', '.col-product', function(item)
 {
 	var img = PollBuilderInject.absoluteURL( item.querySelector('.featured-image').src );
@@ -12,8 +12,13 @@ var doEmbed = PollBuilderInject.map('body.template-collection', '.col-product', 
 	var s = button.style;
 	s.position = 'absolute';
 	s.right = '50%';
-	s.top = '50%';
-	s.transform = 'translate(50%, 50%)';
+	s.bottom = '14px';
+	s.transform = 'translateX(50%)';
+	s.padding = '7px 20px 6px';
+	s.fontWeight = 'bold';
+    s.letterSpacing = '2.5px';
+    s.borderRadius = '5px';
+	s.border = '1px solid #555';
 	s.zIndex = 2;
 	
 	item.querySelector('.grid-link__image-centered').appendChild(button);
